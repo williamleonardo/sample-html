72e4315ced6e0ee831f890e1ef813893f71d4e42
williamleonardo <william.leonardo@mitrais.com>
2017-02-23 16:35:27 +0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,7 +5,7 @@ node {
     env.AWS_DEFAULT_REGION = 'ap-southeast-1'
 	
 	def applicationName = 'hendrawan-app'
-	def deploymentGroupName = 'granit-app-group' // change me
+	def deploymentGroupName = 'william-app-group' // change me
 	def s3BucketName = 'deployment-cdc'
 	
 	//Cleanup workspace
